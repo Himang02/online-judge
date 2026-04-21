657abfce351107483fe9a51d5eb445f5b7fae907
Himang Dongre <hsdongre2002@gmail.com>
2026-04-21 21:16:03 +0530

    feat: add jwt authentication middleware

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -2,7 +2,7 @@
 const authService = require('../services/authService')
 const jwtUtil = require('../utils/jwtUtil');
 
-async function registerUser(req, res) {
+async function registerUser(req, res, next) {
 
     // extract fields from request body
     const { name, username, email, password } = req.body;
@@ -18,11 +18,12 @@ async function registerUser(req, res) {
         return res.status(201).json({ user, token });
     }
     catch (err) {
-        return res.status(err.statusCode || 500).json({ error: err.message });
+        next(err);
     }
+
 }
 
-async function loginUser(req, res) {
+async function loginUser(req, res, next) {
 
     // extract fields from request body
     const { email, password } = req.body;
@@ -38,8 +39,9 @@ async function loginUser(req, res) {
         return res.status(200).json({ user, token });
     }
     catch (err) {
-        return res.status(err.statusCode || 500).json({ error: err.message });
+        next(err);
     }
+
 }
 
 module.exports = {
